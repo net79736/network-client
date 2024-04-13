45d844debd7b2951d59144522e49e98ddffa71de
leejongwook2 <34103253+leejongwook2@users.noreply.github.com>
2024-04-13 09:34:21 +0900

    Initial Commit

---
+++ src/main/java/org/httptest/networkclient/NetworkClientApplication.java
@@ -0,0 +1,13 @@
+package org.httptest.networkclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkClientApplication.class, args);
+    }
+
+}
